663cc13ce739d2f837769d544ad677bd985199df
Marciano Geijselaers <MarcianoG@users.noreply.github.com>
2018-03-06 22:40:15 +0100

    Created new edge creation
    Edge created using keys this time

--- src/graph/mainTest.java
+++ src/graph/mainTest.java
@@ -7,7 +7,7 @@ public class mainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//UndirectedGraph graph = new UndirectedGraphReader().read(new File("E:\\Programming\\Java\\GeneralPurposeCodeJava\\test\\structures\\undirectedGraph\\testFiles\\instance199.gr"));
-		UndirectedGraph graph = new UndirectedGraphReader().read(new File("C:\\Users\\Marciano\\Downloads\\heuristic\\public\\test.gr"));
+		UndirectedGraph graph = new UndirectedGraphReader().read(new File("data\\heuristics\\test.gr"));
                 System.out.println("Number of Vertices: " + graph.getVerticesSize() + " Number of Edges: " + graph.getEdgesSize());
                 PreProcess improved = new PreProcess(graph);
                 System.out.println("Current Degree Scale: ");
@@ -15,8 +15,10 @@ public class mainTest {
                 for(int i = 0; i < degrees.length; i++){
                     System.out.print(degrees[i] + ", ");
                 }
-                improved.removeUnnecessaryVertices();
-                improved.reduceSize();
+                System.out.println("");
+                
+                //improved.removeUnnecessaryVertices();
+                //improved.reduceSize();
                 
                 System.out.println("Number of Vertices: " + improved.graph.getVerticesSize() + " Number of Edges: " + improved.graph.getEdgesSize());
                 System.out.println("");
