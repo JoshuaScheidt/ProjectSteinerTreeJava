37f91b3d1c3eb5b9bd45656f4737c79914e78670
Marciano Geijselaers <marcianogeijselaers@hotmail.com>
2018-03-06 18:30:02 +0100

    Updated and some Testing

--- src/graph/PreProcess.java
+++ src/graph/PreProcess.java
@@ -5,6 +5,7 @@
  */
 package graph;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.HashMap;
@@ -16,22 +17,30 @@ import java.util.HashMap;
 public class PreProcess {
     
     UndirectedGraph graph;
+    
     public PreProcess(UndirectedGraph g){
         this.graph = g.clone();
     }
     
-    
-    public UndirectedGraph reduceSize(){
+    public void reduceSize(){
         Set keys = this.graph.getVertices().keySet();
         Iterator it = keys.iterator();
+        HashMap<Integer, Vertex> vertices = this.graph.getVertices();
         Vertex temp;
+        ArrayList<Vertex> newNeighbors = new ArrayList<>();
+        int combinedCost = 0;
         while(it.hasNext()){
-            temp = (Vertex) it.next();
-            if(temp.getNeighbors().size() == 2){
-                    
+            temp = vertices.get(it.next());
+            if(temp.getNeighbors().size() == 2 && !temp.isTerminal()){
+                for(Edge e : temp.getEdges()){
+                    combinedCost += e.getCost().get();
+                    newNeighbors.add(e.getOtherSide(temp));
+                }
+                this.graph.removeVertex(temp);
+                this.graph.addEdge(newNeighbors.get(0), newNeighbors.get(1), combinedCost);
+                newNeighbors.clear();
             }
         }
-        return null;
     }
     
     public void removeUnnecessaryVertices(){
@@ -39,14 +48,36 @@ public class PreProcess {
         Iterator it = keys.iterator();
         int key;
         HashMap<Integer, Vertex> vertices = this.graph.getVertices();
+        Vertex current, newCurrent;
         while(it.hasNext()) {
             key = (Integer)it.next();
-            Vertex current = vertices.get(key);
-            while(!current.isTerminal() && current.getNeighbors().size() <= 1){
-                Vertex newCurrent = (Vertex) current.getNeighbors().toArray()[0];
+            current = vertices.get(key);
+            while(!current.isTerminal() && current.getNeighbors().size() == 1){
+                newCurrent = (Vertex) current.getNeighbors().toArray()[0];
+                it.remove();
                 this.graph.removeVertex(current);
                 current = newCurrent;
             }
         }
+    }
+    
+    //The methods below are for testing and requesting certain information from the graph
+    public int[] countDegree(){
+        Set keyset = this.graph.getVertices().keySet();
+        HashMap<Integer, Vertex> vertices = this.graph.getVertices();
+        Iterator it = keyset.iterator();
+        int[] degrees = {0,0, 0,0, 0,0, 0,0, 0,0};
+        Vertex temp;
+        int key, number;
+        while(it.hasNext()){
+            key = (Integer) it.next();
+            temp = vertices.get(key);
+            number = temp.getNeighbors().size();
+            if(number >= degrees.length){
+                continue;
+            }
+            degrees[number]++;
+        }
+        return degrees;
     }
 }
--- src/graph/mainTest.java
+++ src/graph/mainTest.java
@@ -6,8 +6,20 @@ public class mainTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		UndirectedGraph graph = new UndirectedGraphReader().read(new File("E:\\Programming\\Java\\GeneralPurposeCodeJava\\test\\structures\\undirectedGraph\\testFiles\\instance199.gr"));
-		
-	}
+		//UndirectedGraph graph = new UndirectedGraphReader().read(new File("E:\\Programming\\Java\\GeneralPurposeCodeJava\\test\\structures\\undirectedGraph\\testFiles\\instance199.gr"));
+		UndirectedGraph graph = new UndirectedGraphReader().read(new File("C:\\Users\\Marciano\\Downloads\\heuristic\\public\\test.gr"));
+                System.out.println("Number of Vertices: " + graph.getVerticesSize() + " Number of Edges: " + graph.getEdgesSize());
+                PreProcess improved = new PreProcess(graph);
+                System.out.println("Current Degree Scale: ");
+                int[] degrees = improved.countDegree();
+                for(int i = 0; i < degrees.length; i++){
+                    System.out.print(degrees[i] + ", ");
+                }
+                improved.removeUnnecessaryVertices();
+                improved.reduceSize();
+                
+                System.out.println("Number of Vertices: " + improved.graph.getVerticesSize() + " Number of Edges: " + improved.graph.getEdgesSize());
+                System.out.println("");
+        }
 
 }
