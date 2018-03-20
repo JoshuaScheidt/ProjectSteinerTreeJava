a63346a3849f3905ea11ce054006262a37952cb4
Joshua Scheidt <j.h.h.scheidt@gmail.com>
2018-03-20 17:00:47 +0100

    Made iterative loop over all files

--- src/graph/UndirectedGraph.java
+++ src/graph/UndirectedGraph.java
@@ -5,7 +5,6 @@
  */
 package graph;
 
-import java.awt.BorderLayout;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -18,275 +17,293 @@ import java.util.Set;
  */
 public class UndirectedGraph {
 
-    private HashMap<Integer, Vertex> vertices = new HashMap<>();
-    private HashSet<Edge> edges = new HashSet<>();
-    private HashMap<Integer, Vertex> terminals = new HashMap<>();
-    private ArrayList<double[]> preliminaryResult = new ArrayList<>();
+	private HashMap<Integer, Vertex> vertices = new HashMap<>();
+	private HashSet<Edge> edges = new HashSet<>();
+	private HashMap<Integer, Vertex> terminals = new HashMap<>();
+	private ArrayList<double[]> preliminaryResult = new ArrayList<>();
 
-    /**
-     * Empty constructor to allow for iterative additions of edges and vertices
-     */
-    public UndirectedGraph() {
-    }
+	/**
+	 * Empty constructor to allow for iterative additions of edges and vertices
+	 */
+	public UndirectedGraph() {
+	}
 
-    /**
-     * Constructor to create a graph from a predefined set of edges and vertices
-     *
-     * @param V Vertices to be added to the new graph
-     * @param E Edges to be added to the new graph
-     */
-    public UndirectedGraph(ArrayList<Vertex> V, ArrayList<Edge> E) {
-        V.forEach((v) -> {
-            this.addVertex(v);
-        });
-        Vertex[] n;
-        for (Edge e : E) {
-            n = e.getVertices();
-            if (this.vertices.containsKey(n[0].getKey()) && this.vertices.containsKey(n[1].getKey())) {
-                this.edges.add(e);
-            } else if (this.vertices.containsKey(((Vertex) n[0]).getKey())) {
-                this.vertices.put(((Vertex) n[1]).getKey(), n[1]);
-                this.edges.add(e);
-            } else {
-                this.vertices.put(((Vertex) n[0]).getKey(), n[0]);
-                this.edges.add(e);
-            }
-        }
-    }
+	/**
+	 * Constructor to create a graph from a predefined set of edges and vertices
+	 *
+	 * @param V
+	 *            Vertices to be added to the new graph
+	 * @param E
+	 *            Edges to be added to the new graph
+	 */
+	public UndirectedGraph(ArrayList<Vertex> V, ArrayList<Edge> E) {
+		V.forEach((v) -> {
+			this.addVertex(v);
+		});
+		Vertex[] n;
+		for (Edge e : E) {
+			n = e.getVertices();
+			if (this.vertices.containsKey(n[0].getKey()) && this.vertices.containsKey(n[1].getKey())) {
+				this.edges.add(e);
+			} else if (this.vertices.containsKey(n[0].getKey())) {
+				this.vertices.put(n[1].getKey(), n[1]);
+				this.edges.add(e);
+			} else {
+				this.vertices.put(n[0].getKey(), n[0]);
+				this.edges.add(e);
+			}
+		}
+	}
 
-    /**
-     * This method adds a vertex to the graph it also checks if it already
-     * contains this vertex if it does it will not add it
-     *
-     * @param N The vertex to be added
-     */
-    public void addVertex(Vertex N) {
-        if (!(N instanceof Vertex) || this.vertices.containsValue((Vertex) N)) {
-            return;
-        }
-        this.vertices.put(((Vertex) N).getKey(), N);
-    }
+	/**
+	 * This method adds a vertex to the graph it also checks if it already contains
+	 * this vertex if it does it will not add it
+	 *
+	 * @param N
+	 *            The vertex to be added
+	 */
+	public void addVertex(Vertex N) {
+		if (!(N instanceof Vertex) || this.vertices.containsValue(N)) {
+			return;
+		}
+		this.vertices.put(N.getKey(), N);
+	}
 
-    /**
-     * Method for constructing edges using only keys.
-     * It checks if these keys have been used and else will use the initialised Vertex
-     * If neither has been initialised this will be done
-     * @param key1 Key of the first Vertex
-     * @param key2 Key of the second Vertex
-     * @param cost Cost of the Edge between the Vertices
-     */
-    public Edge addEdge(int key1, int key2, double cost){
-        Edge e = null;
-        if (this.vertices.containsKey(key1) || this.vertices.containsKey(key2)) {
-            if (this.vertices.containsKey(key1) && this.vertices.containsKey(key2)) {
-                e = new Edge(this.vertices.get(key1), this.vertices.get(key2), cost);
-                this.edges.add(e);
-            } else if (this.vertices.containsKey(key1)) {
-                this.vertices.put(key2, new Vertex(key2));
-                e = new Edge(this.vertices.get(key1), this.vertices.get(key2), cost);
-                this.edges.add(e);
-            } else if (this.vertices.containsKey(key2)) {
-                this.vertices.put(key1, new Vertex(key1));
-                e = new Edge(this.vertices.get(key1), this.vertices.get(key2), cost);
-                this.edges.add(e);
-            }
-        } else {
-            this.vertices.put(key1, new Vertex(key1));
-            this.vertices.put(key2, new Vertex(key2));
-            e = new Edge(this.vertices.get(key1), this.vertices.get(key2), cost);
-            this.edges.add(e);
-        }
-        return e;
-    }
-    
-    /**
-     * Adds edge to the graph given 2 vertices and a cost It will add vertices
-     * to the graph if they weren't already in there
-     *
-     * @param v1 First Vertex to be added
-     * @param v2 Second Vertex to be added
-     * @param cost Cost of the Edge
-     * @return The created Edge
-     */
-    public Edge addEdge(Vertex v1, Vertex v2, int cost) {
-        Edge e = null;
-        if (this.vertices.containsValue(v1) || this.vertices.containsValue(v2)) {
-            if (this.vertices.containsValue(v1) && this.vertices.containsValue(v2)) {
-                e = new Edge(v1, v2, cost);
-                this.edges.add(e);
-            } else if (this.vertices.containsValue(v1)) {
-                this.vertices.put((v2).getKey(), v2);
-                e = new Edge(v1, v2, cost);
-                this.edges.add(e);
-            } else if (this.vertices.containsValue(v2)) {
-                this.vertices.put((v1).getKey(), v1);
-                e = new Edge(v1, v2, cost);
-                this.edges.add(e);
-            }
-        } else {
-            this.vertices.put((v1).getKey(), v1);
-            this.vertices.put((v2).getKey(), v2);
-            e = new Edge(v1, v2, cost);
-            this.edges.add(e);
-        }
-        return e;
-    }
+	/**
+	 * Method for constructing edges using only keys. It checks if these keys have
+	 * been used and else will use the initialised Vertex If neither has been
+	 * initialised this will be done
+	 * 
+	 * @param key1
+	 *            Key of the first Vertex
+	 * @param key2
+	 *            Key of the second Vertex
+	 * @param cost
+	 *            Cost of the Edge between the Vertices
+	 */
+	public Edge addEdge(int key1, int key2, double cost) {
+		Edge e = null;
+		if (this.vertices.containsKey(key1) || this.vertices.containsKey(key2)) {
+			if (this.vertices.containsKey(key1) && this.vertices.containsKey(key2)) {
+				e = new Edge(this.vertices.get(key1), this.vertices.get(key2), cost);
+				this.edges.add(e);
+			} else if (this.vertices.containsKey(key1)) {
+				this.vertices.put(key2, new Vertex(key2));
+				e = new Edge(this.vertices.get(key1), this.vertices.get(key2), cost);
+				this.edges.add(e);
+			} else if (this.vertices.containsKey(key2)) {
+				this.vertices.put(key1, new Vertex(key1));
+				e = new Edge(this.vertices.get(key1), this.vertices.get(key2), cost);
+				this.edges.add(e);
+			}
+		} else {
+			this.vertices.put(key1, new Vertex(key1));
+			this.vertices.put(key2, new Vertex(key2));
+			e = new Edge(this.vertices.get(key1), this.vertices.get(key2), cost);
+			this.edges.add(e);
+		}
+		return e;
+	}
 
-    /**
-     * This method sets the terminals for the existing vertices in the graph
-     *
-     * @param keys The keys for which vertex is a terminal
-     */
-    public void setTerminals(Set<Integer> keys) {
-        for (Integer key : keys) {
-            if (this.vertices.containsKey(key)) {
-                this.vertices.get(key).setTerminal(true);
-                this.terminals.put(key, this.vertices.get(key));
-            } else {
-                System.out.println("Terminal appointed to non-existing Vertex");
-            }
-        }
-    }
-    
-    /**
-     * This method sets an individual Vertex to be a terminal
-     * 
-     * @param key Key of Vertex to be made terminal
-     */
-    public void setTerminal(int key){
-        if(this.vertices.containsKey(key)){
-            this.vertices.get(key).setTerminal(true);
-            this.terminals.put(key, this.vertices.get(key));
-        } else {
-            System.out.println("Terminal appointed to non-existing Vertex");
-        }
-    }
+	/**
+	 * Adds edge to the graph given 2 vertices and a cost It will add vertices to
+	 * the graph if they weren't already in there
+	 *
+	 * @param v1
+	 *            First Vertex to be added
+	 * @param v2
+	 *            Second Vertex to be added
+	 * @param cost
+	 *            Cost of the Edge
+	 * @return The created Edge
+	 */
+	public Edge addEdge(Vertex v1, Vertex v2, int cost) {
+		Edge e = null;
+		if (this.vertices.containsValue(v1) || this.vertices.containsValue(v2)) {
+			if (this.vertices.containsValue(v1) && this.vertices.containsValue(v2)) {
+				e = new Edge(v1, v2, cost);
+				this.edges.add(e);
+			} else if (this.vertices.containsValue(v1)) {
+				this.vertices.put((v2).getKey(), v2);
+				e = new Edge(v1, v2, cost);
+				this.edges.add(e);
+			} else if (this.vertices.containsValue(v2)) {
+				this.vertices.put((v1).getKey(), v1);
+				e = new Edge(v1, v2, cost);
+				this.edges.add(e);
+			}
+		} else {
+			this.vertices.put((v1).getKey(), v1);
+			this.vertices.put((v2).getKey(), v2);
+			e = new Edge(v1, v2, cost);
+			this.edges.add(e);
+		}
+		return e;
+	}
 
-    /**
-     * Clone method to provide a deepclone of the current graph
-     *
-     * @return An exact clone of the original called graph
-     */
-    public UndirectedGraph clone() {
-        UndirectedGraph graph = new UndirectedGraph();
-        for (Edge e : this.edges) {
-            graph.addEdge(e.getVertices()[0].getKey(), e.getVertices()[1].getKey(), e.getCost().get());
-        }
-        for (int key : this.terminals.keySet()) {
-            (graph.vertices.get(key)).setTerminal(true);
-        }
-        graph.setTerminals(this.terminals.keySet());
-        return graph;
-    }
+	/**
+	 * This method sets the terminals for the existing vertices in the graph
+	 *
+	 * @param keys
+	 *            The keys for which vertex is a terminal
+	 */
+	public void setTerminals(Set<Integer> keys) {
+		for (Integer key : keys) {
+			if (this.vertices.containsKey(key)) {
+				this.vertices.get(key).setTerminal(true);
+				this.terminals.put(key, this.vertices.get(key));
+			} else {
+				System.out.println("Terminal appointed to non-existing Vertex");
+			}
+		}
+	}
 
-    /**
-     * This method removes an Edge from each of its connections Beware it
-     * doesn't check connectivity or create a new edge
-     *
-     * @param e The edge to be removed
-     */
-    public void removeEdge(Edge e) {
-        for (Vertex v : e.getVertices()) {
-            v.getEdges().remove(e);
-        }
-        this.edges.remove(e);
-        e = null;
-    }
+	/**
+	 * This method sets an individual Vertex to be a terminal
+	 * 
+	 * @param key
+	 *            Key of Vertex to be made terminal
+	 */
+	public void setTerminal(int key) {
+		if (this.vertices.containsKey(key)) {
+			this.vertices.get(key).setTerminal(true);
+			this.terminals.put(key, this.vertices.get(key));
+		} else {
+			System.out.println("Terminal appointed to non-existing Vertex");
+		}
+	}
 
-    /**
-     * Removes a vertex from the graph this included removing all its edge
-     * connection and removing all of these edges from its neighbors
-     *
-     * @param v The vertex to be removed
-     */
-    public void removeVertex(Vertex v) {
-        for (Edge e : v.getEdges()) {
-            this.edges.remove(e);
-        }
-        int key = v.getKey();
-        if (v.isTerminal()) {
-            this.terminals.keySet().remove(key);
-        }
-        this.vertices.keySet().remove(key);
-        v = null;
-    }
-    
-    /**
-     * Removes a vertex from the graph this included removing all its edge
-     * connection and removing all of these edges from its neighbours
-     *
-     * @param key The key the to be removed vertex has
-     */
-    public void removeVertex(int key){
-        Vertex v = this.getVertices().get(key);
-        HashSet<Edge> toBeRemoved = new HashSet<>();
-        for (Edge e : v.getEdges()) {
-            toBeRemoved.add(e);
-            this.edges.remove(e);
-        }
-        for(Edge e : toBeRemoved){
-            v.removeEdge(e);
-            e.getOtherSide(v).removeEdge(e);
-            e = null;
-        }
-        if (v.isTerminal()) {
-            this.terminals.keySet().remove(key);
-        }
-        this.vertices.remove(key);
-        v = null;
-    }
+	/**
+	 * Clone method to provide a deepclone of the current graph
+	 *
+	 * @return An exact clone of the original called graph
+	 */
+	@Override
+	public UndirectedGraph clone() {
+		UndirectedGraph graph = new UndirectedGraph();
+		for (Edge e : this.edges) {
+			graph.addEdge(e.getVertices()[0].getKey(), e.getVertices()[1].getKey(), e.getCost().get());
+		}
+		for (int key : this.terminals.keySet()) {
+			(graph.vertices.get(key)).setTerminal(true);
+		}
+		graph.setTerminals(this.terminals.keySet());
+		return graph;
+	}
 
-    /**
-     * Returns the HashMap of the Vertices in the current graph
-     *
-     * @return The HashMap in question
-     */
-    public HashMap<Integer, Vertex> getVertices() {
-        return this.vertices;
-    }
+	/**
+	 * This method removes an Edge from each of its connections Beware it doesn't
+	 * check connectivity or create a new edge
+	 *
+	 * @param e
+	 *            The edge to be removed
+	 */
+	public void removeEdge(Edge e) {
+		for (Vertex v : e.getVertices()) {
+			v.getEdges().remove(e);
+		}
+		this.edges.remove(e);
+		e = null;
+	}
 
-    /**
-     * Returns the HashMap of the Edges in the current graph
-     *
-     * @return The HashMap in question
-     */
-    public HashSet<Edge> getEdges() {
-        return this.edges;
-    }
+	/**
+	 * Removes a vertex from the graph this included removing all its edge
+	 * connection and removing all of these edges from its neighbors
+	 *
+	 * @param v
+	 *            The vertex to be removed
+	 */
+	public void removeVertex(Vertex v) {
+		for (Edge e : v.getEdges()) {
+			this.edges.remove(e);
+		}
+		int key = v.getKey();
+		if (v.isTerminal()) {
+			this.terminals.keySet().remove(key);
+		}
+		this.vertices.keySet().remove(key);
+		v = null;
+	}
 
-    /**
-     * Get the number of terminal Vertices in the current graph
-     *
-     * @return Non-negative integer which holds the number of terminals
-     */
-    public int getNumberOfTerminals() {
-        return this.terminals.size();
-    }
+	/**
+	 * Removes a vertex from the graph this included removing all its edge
+	 * connection and removing all of these edges from its neighbours
+	 *
+	 * @param key
+	 *            The key the to be removed vertex has
+	 */
+	public void removeVertex(int key) {
+		Vertex v = this.getVertices().get(key);
+		HashSet<Edge> toBeRemoved = new HashSet<>();
+		for (Edge e : v.getEdges()) {
+			toBeRemoved.add(e);
+			this.edges.remove(e);
+		}
+		for (Edge e : toBeRemoved) {
+			v.removeEdge(e);
+			e.getOtherSide(v).removeEdge(e);
+			e = null;
+		}
+		if (v.isTerminal()) {
+			this.terminals.keySet().remove(key);
+		}
+		this.vertices.remove(key);
+		v = null;
+	}
 
-    //The methods below are for testing and requesting certain information from the graph
-    public int[] countDegree(){
-        Set keyset = this.getVertices().keySet();
-        HashMap<Integer, Vertex> vertices = this.getVertices();
-        Iterator it = keyset.iterator();
-        int[] degrees = {0,0, 0,0, 0,0, 0,0, 0,0};
-        Vertex temp;
-        int key, number;
-        while(it.hasNext()){
-            key = (Integer) it.next();
-            temp = vertices.get(key);
-            number = temp.getNeighbors().size();
-            if(number >= degrees.length+1){
-                continue;
-            }
-            degrees[number-1]++;
-        }
-        return degrees;
-    }
-    
-    public Edge edgeBetweenVertices(Vertex v, Vertex u) throws GraphException {
-    	for(Edge e : v.getEdges()) {
-    		if(e.getOtherSide(v) == u) return e;
-    	}
-    	throw new GraphException("No edges from v contain vertex u.");
-    }
+	/**
+	 * Returns the HashMap of the Vertices in the current graph
+	 *
+	 * @return The HashMap in question
+	 */
+	public HashMap<Integer, Vertex> getVertices() {
+		return this.vertices;
+	}
+
+	/**
+	 * Returns the HashMap of the Edges in the current graph
+	 *
+	 * @return The HashMap in question
+	 */
+	public HashSet<Edge> getEdges() {
+		return this.edges;
+	}
+
+	/**
+	 * Get the number of terminal Vertices in the current graph
+	 *
+	 * @return Non-negative integer which holds the number of terminals
+	 */
+	public int getNumberOfTerminals() {
+		return this.terminals.size();
+	}
+
+	// The methods below are for testing and requesting certain information from the
+	// graph
+	public int[] countDegree() {
+		Set keyset = this.getVertices().keySet();
+		HashMap<Integer, Vertex> vertices = this.getVertices();
+		Iterator it = keyset.iterator();
+		int[] degrees = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		Vertex temp;
+		int key, number;
+		while (it.hasNext()) {
+			key = (Integer) it.next();
+			temp = vertices.get(key);
+			number = temp.getNeighbors().size();
+			if (number >= degrees.length + 1) {
+				continue;
+			}
+			degrees[number - 1]++;
+		}
+		return degrees;
+	}
+
+	public Edge edgeBetweenVertices(Vertex v, Vertex u) throws GraphException {
+		for (Edge e : v.getEdges()) {
+			if (e.getOtherSide(v) == u)
+				return e;
+		}
+		throw new GraphException("No edges from v contain vertex u.");
+	}
 }
--- src/graph/mainTest.java
+++ src/graph/mainTest.java
@@ -2,65 +2,115 @@ package graph;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class mainTest {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        UndirectedGraph graph = new UndirectedGraphReader().read(new File("data\\heuristics\\instance197.gr"));
-        PreProcess improved = new PreProcess(graph);
+	public static void main(String[] args) {
 
-        printCurrentSize(improved);
-        System.out.println("Original Degree Scale: ");
-        int[] degrees = graph.countDegree();
-        for (int i = 0; i < degrees.length; i++) {
-            System.out.print(degrees[i] + ", ");
-        }
-        System.out.println("");
+		File[] files = readFiles(new File("data\\heuristics\\instance197.gr"));
 
-        //Bridge Finding
-//		long start = System.currentTimeMillis();
-//		ArrayList<Edge> bridges = improved.tarjanBridgeFinding();
-//		System.out.println("Time needed: " + (System.currentTimeMillis() - start) + " ms");
-//		System.out.println("Found " + bridges.size() + " bridges.");
-//		for (Edge e : bridges)
-//			System.out.println("Bridge found on vertices: " + e.getVertices()[0].getKey() + " and  " + e.getVertices()[1].getKey());
-        // System.out.println("done");
-        //Leaf Node Removal
-        improved.removeLeafNodes();
+		Integer[][][] results = new Integer[files.length][1][3]; // Per file, save all different graphs' Nodes, Terminals and Edges. The second
+																	// index has to be changed depending on which comparisons we want. The first
+																	// index will always be the base graph without preprocess changes.
 
-        //Non-Terminal Degree Two removal
-        long start = System.nanoTime();
-        improved.removeNonTerminalDegreeTwo();
-        long stop = System.nanoTime();
-        System.out.println("Time Taken: " + (stop - start) / 1000000000.0);
+		for (int fileIndex = 0; fileIndex < files.length; fileIndex++) {
+			// Read the standard graph and set its values to the first index of the results.
+			UndirectedGraph graph = new UndirectedGraphReader().read(files[fileIndex]);
+			results[fileIndex][0][0] = graph.getVertices().size();
+			results[fileIndex][0][1] = graph.getNumberOfTerminals();
+			results[fileIndex][0][2] = graph.getEdges().size();
 
-        printCurrentSize(improved);
-        printDegreeScale(improved);
-        
-        improved.removeLeafNodes();
+			// Create a cloned graph which will use preprocessing.
+			PreProcess improved = new PreProcess(graph);
+			printCurrentSize(improved);
 
-        printCurrentSize(improved);
-        printDegreeScale(improved);
-        
-        improved.removeNonTerminalDegreeTwo();
-        
-    }
+			// Prints the degrees of the vertices from the original graph up to a maximum of
+			// degree 9.
+			System.out.println("Original Degree Scale: ");
+			int[] degrees = graph.countDegree();
+			for (int i = 0; i < degrees.length; i++) {
+				System.out.print(degrees[i] + ", ");
+			}
+			System.out.println("");
 
-    public static void printCurrentSize(PreProcess improved) {
-        System.out.println(
-                "Current number of (Vertices, Terminals): ("
-                + improved.graph.getVertices().size() + ", " + improved.graph.getNumberOfTerminals()
-                + ") Current number of Edges: " + improved.graph.getEdges().size());
-    }
+			// Bridge Finding
+			// long start = System.currentTimeMillis();
+			// ArrayList<Edge> bridges = improved.tarjanBridgeFinding();
+			// System.out.println("Time needed: " + (System.currentTimeMillis() - start) + "
+			// ms");
+			// System.out.println("Found " + bridges.size() + " bridges.");
+			// for (Edge e : bridges)
+			// System.out.println("Bridge found on vertices: " + e.getVertices()[0].getKey()
+			// + " and " + e.getVertices()[1].getKey());
+			// System.out.println("done");
 
-    public static void printDegreeScale(PreProcess improved) {
-        System.out.println("Current Degree Scale: ");
-        int[] clonedDegrees = improved.graph.countDegree();
-        for (int i = 0; i < clonedDegrees.length; i++) {
-            System.out.print(clonedDegrees[i] + ", ");
-        }
-        System.out.println("");
-    }
+			// Leaf Node Removal
+			improved.removeLeafNodes();
+
+			// Non-Terminal Degree Two removal
+			long start = System.nanoTime();
+			improved.removeNonTerminalDegreeTwo();
+			long stop = System.nanoTime();
+			System.out.println("Time Taken: " + (stop - start) / 1000000000.0);
+
+			printCurrentSize(improved);
+			printDegreeScale(improved);
+
+			improved.removeLeafNodes();
+
+			printCurrentSize(improved);
+			printDegreeScale(improved);
+
+			improved.removeNonTerminalDegreeTwo();
+		}
+		System.out.println("\n\nTotal results:");
+		for (Integer[][] singleFileResults : results) {
+			System.out.println(Arrays.toString(singleFileResults[0]));
+		}
+
+	}
+
+	/**
+	 * Reads all files from a given directory. The directory is allowed to be both a
+	 * folder or a file.
+	 *
+	 * @param directory
+	 *            The directory for which file(s) have to be read.
+	 * @return An array of found files.
+	 *
+	 * @author Joshua Scheidt
+	 */
+	private static File[] readFiles(File directory) {
+		if (directory.exists()) {
+			if (directory.isFile() && directory.getName().contains(".gr")) {
+				return new File[] { directory };
+			} else if (directory.isDirectory()) {
+				File[] files = directory.listFiles();
+				ArrayList<File> filesList = new ArrayList<>();
+				for (int i = 0; i < files.length; i++) {
+					if (files[i].isFile() && files[i].getName().contains(".gr")) {
+						filesList.add(files[i]);
+					}
+				}
+				return filesList.toArray(files);
+			}
+		}
+		return new File[] {};
+	}
+
+	public static void printCurrentSize(PreProcess improved) {
+		System.out.println("Current number of (Vertices, Terminals): (" + improved.graph.getVertices().size() + ", "
+				+ improved.graph.getNumberOfTerminals() + ") Current number of Edges: " + improved.graph.getEdges().size());
+	}
+
+	public static void printDegreeScale(PreProcess improved) {
+		System.out.println("Current Degree Scale: ");
+		int[] clonedDegrees = improved.graph.countDegree();
+		for (int i = 0; i < clonedDegrees.length; i++) {
+			System.out.print(clonedDegrees[i] + ", ");
+		}
+		System.out.println("");
+	}
 
 }
