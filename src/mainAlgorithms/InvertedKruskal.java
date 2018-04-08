a19afd7f6d2c0f656b5a5e47c6afa1131c2c4459
Marciano Geijselaers <marcianogeijselaers@hotmail.com>
2018-04-08 19:21:44 +0200

    Sorter implemented
    mergeSort for speed, insertion for memory consumption

--- src/mainAlgorithms/InvertedKruskal.java
+++ src/mainAlgorithms/InvertedKruskal.java
@@ -1,6 +1,9 @@
 package mainAlgorithms;
 
+import graph.Edge;
 import graph.UndirectedGraph;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,7 +18,91 @@ import graph.UndirectedGraph;
 public class InvertedKruskal {
     
     private UndirectedGraph g;
+    private ArrayList<Edge> sorted;
+    private HashMap<Integer, Boolean> connected;
+    
     public InvertedKruskal(UndirectedGraph g){
         this.g = g;
+        this.sorted = new ArrayList<>();
+        this.connected = new HashMap<>();
+    }
+    
+    public void start(){
+        this.createSortedEdgeList();
+    }
+    
+    public void createSortedEdgeList(){
+        this.sorted.addAll(this.g.getEdges());
+        
+        System.out.println(this.sorted.size());
+        
+//        this.sorted = this.mergeSort(sorted);
+        this.sorted = this.insertionSort(sorted);
+        
+        System.out.println(this.sorted.size());
+        
+        System.out.print("[");
+        for(int i = 0; i < this.sorted.size(); i++){
+            System.out.print(this.sorted.get(i).getCost().get() + ", ");
+        }
+        System.out.println("]");
+    }
+    
+    public ArrayList<Edge> insertionSort(ArrayList<Edge> x){
+        ArrayList<Edge> sorted = new ArrayList<>();
+        sorted.add(x.get(0));
+        for(int i = 1; i < x.size(); i++){
+            for(int j = 0; j < sorted.size(); j++){
+                if(x.get(i).getCost().get() < sorted.get(j).getCost().get()){
+                    if(j == sorted.size() - 1){
+                        sorted.add(x.get(i));
+                        break;
+                    }
+                } else {
+                    sorted.add(j, x.get(i));
+                    break;
+                }
+            }   
+        }
+        return sorted;
+    }
+    public ArrayList<Edge> mergeSort(ArrayList<Edge> x){
+        if(x.size() == 1){
+            return x;
+        } else {
+            ArrayList<Edge> a = new ArrayList<>(), b = new ArrayList<>();
+            for(int i = 0; i < x.size(); i++){
+                if(i < x.size()/2){
+                    a.add(x.get(i));
+                } else {
+                    b.add(x.get(i));
+                }
+            }
+            a = mergeSort(a);
+            b = mergeSort(b);
+            ArrayList<Edge> sorted = new ArrayList<>();
+            outerloop: for(int i = 0; i < a.size(); i++){
+                for(int j = 0; j < b.size(); j++){
+                    if(a.get(i).getCost().get() > b.get(j).getCost().get()){
+                        sorted.add(a.get(i));
+                        if(i == a.size()-1){
+                            for(int k = j; k < b.size(); k++){
+                                sorted.add(b.get(k));
+                            }
+                        }
+                        break;
+                    } else {
+                        sorted.add(b.get(j));
+                        if(j == b.size()-1){
+                            for(int k = i; k < a.size(); k++){
+                                sorted.add(a.get(k));
+                            }
+                            break outerloop;
+                        }
+                    }
+                }
+            }
+            return sorted;
+        }
     }
 }
--- src/mixedIntegerProgramming/ILPTester.java
+++ src/mixedIntegerProgramming/ILPTester.java
@@ -8,6 +8,7 @@ package mixedIntegerProgramming;
 import graph.UndirectedGraph;
 import graph.UndirectedGraphReader;
 import java.io.File;
+import mainAlgorithms.InvertedKruskal;
 
 /**
  *
@@ -16,13 +17,20 @@ import java.io.File;
 public class ILPTester {
     
     public static void main(String[] args){
-        String fileName = "data\\heuristics\\instance039.gr";
+        String fileName = "data\\heuristics\\instance197.gr";
         File file = new File(fileName);
         fileName = fileName.substring(fileName.indexOf("\\") + 1);
         fileName = fileName.substring(fileName.indexOf("\\") + 1);
         fileName = fileName.substring(0, fileName.indexOf("."));
         UndirectedGraph graph = new UndirectedGraphReader().read(file);
-        CutILP fp = new CutILP(graph, fileName);
-        fp.initiateCutSearch();
+//        CutILP fp = new CutILP(graph, fileName);
+//        fp.initiateCutSearch();
+        long starts = System.currentTimeMillis();
+        
+        InvertedKruskal ik = new InvertedKruskal(graph);
+        ik.start();
+
+        System.out.println("Took " + (System.currentTimeMillis() - starts) + " ms");
+
     }
 }
