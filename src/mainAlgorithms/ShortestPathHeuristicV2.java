a7474c540df839a1269d9b4d16cade218a1769a3
Joshua Scheidt <j.h.h.scheidt@gmail.com>
2018-06-12 19:12:25 +0200

    finished faster SPH

--- src/mainAlgorithms/ShortestPathHeuristicV2.java
+++ src/mainAlgorithms/ShortestPathHeuristicV2.java
@@ -5,9 +5,10 @@
 package mainAlgorithms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Stack;
 
 import graph.Edge;
@@ -26,7 +27,7 @@ public class ShortestPathHeuristicV2 implements SteinerTreeSolver {
 
 	public static class DijkstraInfo {
 		public int dist;
-		public Vertex parent = null;
+		public Integer parent = -1;
 
 		public DijkstraInfo(int dist) {
 			this.dist = dist;
@@ -35,19 +36,26 @@ public class ShortestPathHeuristicV2 implements SteinerTreeSolver {
 
 	@Override
 	public List<Edge> solve(UndirectedGraph G) {
-		System.out.println("in here");
 		this.graph = G;
 		List<Edge> bestResult = null;
 		int bestVal = Integer.MAX_VALUE;
-		for (int t : this.graph.getTerminals().keySet()) {
-			System.out.println(Arrays.toString(this.dijkstraOrdering(t).toArray()));
-			// List<Edge> result = this.dijkstraPathFinder(this.dijkstraOrdering(t));
-			// int val = 0;
-			// for (Edge e : result)
-			// val += e.getCost().get();
-			// if (val < bestVal)
-			// bestResult = result;
+		List<Integer> terminals = new ArrayList<>();
+		for (int i : this.graph.getTerminals().keySet())
+			terminals.add(i);
+		Random rand = new Random(42);
+		while (terminals.size() != 0) {
+			int t = terminals.remove(rand.nextInt(terminals.size()));
+			List<Edge> result = this.dijkstraPathFinder(this.dijkstraOrdering(t));
+			int val = 0;
+			for (Edge e : result)
+				val += e.getCost().get();
+			System.out.println("Terminal " + t + " scores " + val);
+			if (val < bestVal) {
+				bestResult = result;
+				bestVal = val;
+			}
 		}
+		System.out.println("Best val: " + bestVal);
 		return bestResult;
 	}
 
@@ -113,70 +121,42 @@ public class ShortestPathHeuristicV2 implements SteinerTreeSolver {
 	 *            the ordering of the terminals
 	 * @return
 	 *
-	 * @author Marciano Geijselaers
 	 * @author Joshua Scheidt
 	 */
 	private List<Edge> dijkstraPathFinder(ArrayList<Integer> ordering) {
-		return null;
-
-	}
-
-	/**
-	 * Performs Dijkstra's path finding algorithm and returns the new edges between
-	 * the vertices.
-	 *
-	 * @param G
-	 *            The graph in which Dijkstra has to be performed
-	 * @param start
-	 *            The starting vertex
-	 * @param end
-	 *            The endpoint vertices as an array
-	 * @param edges
-	 *            The allowed edges to traverse over
-	 * @return The new edges with the lowest weights
-	 *
-	 * @author Joshua Scheidt
-	 */
-	public static ArrayList<Edge> DijkstraMultiPath(UndirectedGraph G, Vertex start, ArrayList<Vertex> end, ArrayList<Edge> edges) {
-		ArrayList<Vertex> Q = new ArrayList<>();
-		HashMap<Vertex, DijkstraInfo> datamap = new HashMap<>();
-		for (Edge e : edges) {
-			if (!datamap.containsKey(e.getVertices()[0])) {
-				datamap.put(e.getVertices()[0], new DijkstraInfo(Integer.MAX_VALUE));
-				Q.add(e.getVertices()[0]);
+		HashSet<Integer> solNodes = new HashSet<>();
+		List<Edge> result = new ArrayList<>();
+		solNodes.add(ordering.get(0));
+		for (int i = 1; i < ordering.size(); i++) {
+			ArrayList<Integer> Q = new ArrayList<>();
+			HashMap<Integer, DijkstraInfo> datamap = new HashMap<>();
+			for (Integer v : this.graph.getVertices().keySet()) {
+				datamap.put(v, new DijkstraInfo(Integer.MAX_VALUE));
+				Q.add(v);
 			}
-			if (!datamap.containsKey(e.getVertices()[1])) {
-				datamap.put(e.getVertices()[1], new DijkstraInfo(Integer.MAX_VALUE));
-				Q.add(e.getVertices()[1]);
-			}
-		}
-		datamap.get(start).dist = 0;
-
-		int numReachedEnd = 0;
-		// System.out.println(G.getVertices().get(5).getNeighbors().size());
-		while (!Q.isEmpty()) {
-			int smallestDist = Integer.MAX_VALUE;
-			Vertex current = null;
-			for (Vertex i : Q) {
-				if (datamap.get(i).dist < smallestDist) {
-					current = i;
-					smallestDist = datamap.get(i).dist;
+			datamap.get(ordering.get(i)).dist = 0;
+			int foundEnd = -1;
+			while (!Q.isEmpty()) {
+				int smallestDist = Integer.MAX_VALUE;
+				Integer current = -1;
+				for (Integer v : Q) {
+					if (datamap.get(v).dist < smallestDist) {
+						current = v;
+						smallestDist = datamap.get(v).dist;
+					}
 				}
-			}
-			if (numReachedEnd == end.size())
-				break;
-			if (current == null)
-				System.out.println("ERROR: No shortest distance vertex found with distance < INTEGER.MAX_VALUE");
-			for (Vertex i : end)
-				if (i.getKey() == current.getKey())
-					numReachedEnd++;
-			Q.remove(current);
-			int distToCur = datamap.get(current).dist;
-			int totDistToNb = 0;
-			for (Vertex nb : current.getNeighbors()) {
-				if (edges.contains(current.getConnectingEdge(nb))) {
-					totDistToNb = distToCur + current.getConnectingEdge(nb).getCost().get();
-					DijkstraInfo nbInfo = datamap.get(nb);
+				if (current == null)
+					System.out.println("ERROR: No shortest distance vertex found with distance < INTEGER.MAX_VALUE");
+				if (solNodes.contains(current)) {
+					foundEnd = current;
+					break;
+				}
+				Q.remove(current);
+				int distToCur = datamap.get(current).dist;
+				int totDistToNb = 0;
+				for (Vertex nb : this.graph.getVertices().get(current).getNeighbors()) {
+					totDistToNb = distToCur + this.graph.getVertices().get(current).getConnectingEdge(nb).getCost().get();
+					DijkstraInfo nbInfo = datamap.get(nb.getKey());
 					if (nbInfo == null)
 						System.out.println(nb.getKey() + " ???");
 					if (totDistToNb < nbInfo.dist) {
@@ -184,37 +164,26 @@ public class ShortestPathHeuristicV2 implements SteinerTreeSolver {
 						nbInfo.parent = current;
 					}
 				}
+
 			}
-
-		}
-
-		ArrayList<Edge> result = new ArrayList<>();
-		for (Vertex v : end) {
-			ArrayList<Vertex> path = new ArrayList<>();
-			Vertex current = v;
-			while (datamap.get(current).parent != null) {
-				path.add(current);
+			Integer current = foundEnd;
+			while (datamap.get(current).parent != -1) {
+				solNodes.add(current);
+				if (this.graph.getVertices().get(current).getConnectingEdge(this.graph.getVertices().get(datamap.get(current).parent))
+						.getStack() != null) {
+					Stack<int[]> stack = this.graph.getVertices().get(current)
+							.getConnectingEdge(this.graph.getVertices().get(datamap.get(current).parent)).getStack();
+					for (int j = 0; j < stack.size(); j++) {
+						solNodes.add(stack.get(j)[0]);
+						result.add(this.graph.getVertices().get(stack.get(j)[0]).getConnectingEdge(this.graph.getVertices().get(stack.get(j)[1])));
+					}
+				} else
+					result.add(this.graph.getVertices().get(current).getConnectingEdge(this.graph.getVertices().get(datamap.get(current).parent)));
 				current = datamap.get(current).parent;
 			}
-			path.add(current);
-			if (start.isNeighbor(v) && start.getConnectingEdge(v).getCost().get() <= datamap.get(v).dist)
-				continue;
-			else {
-				Stack<int[]> pathStack = new Stack<>();
-				for (int i = 0; i < path.size() - 1; i++) {
-					pathStack.push(new int[] { path.get(i).getKey(), path.get(i + 1).getKey(), G.getVertices().get(path.get(i).getKey())
-							.getConnectingEdge(G.getVertices().get(path.get(i + 1).getKey())).getCost().get() });
-				}
-				if (start.isNeighbor(v)) {
-					start.getConnectingEdge(v).setCost(datamap.get(v).dist);
-					start.getConnectingEdge(v).replaceStack(pathStack);
-				} else {
-					Edge newEdge = new Edge(start, v, datamap.get(v).dist);
-					newEdge.pushStack(pathStack);
-					result.add(newEdge);
-				}
-			}
+			solNodes.add(current);
 		}
 		return result;
 	}
+
 }
